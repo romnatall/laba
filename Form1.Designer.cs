e20671f59e7c41354d467477a979629ace725b11
429284-15 <429284-15>
2022-02-18 15:11:41 +0300

    first

---
+++ Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(200, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "";
+            this.dateTimePicker1.Location = new System.Drawing.Point(12, 39);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 1;
+            this.dateTimePicker1.Value = new System.DateTime(2021, 12, 28, 0, 0, 0, 0);
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(12, 65);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker2.TabIndex = 2;
+            // 
+            // chart1
+            // 
+            this.chart1.BorderlineWidth = 5;
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(258, 12);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(494, 364);
+            this.chart1.TabIndex = 3;
+            this.chart1.Text = "chart1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 111);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 66);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.comboBox1);
+            this.MinimumSize = new System.Drawing.Size(816, 489);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using System.Globalization;
+using System.Windows.Forms.DataVisualization.Charting;
+
+
+namespace WindowsFormsApp1
+{
+ 
+    public partial class Form1 : Form
+    {
+        public class ComboBoxItem
+        {
+            string _Contents;
+            public string Contents
+            {
+                get { return _Contents; }
+                set { _Contents = value; }
+            }
+            object _Tag;
+            public object Tag
+            {
+                get { return _Tag; }
+                set { _Tag = value; }
+            }
+            public ComboBoxItem(string contents, object tag)
+            {
+                this._Contents = contents;
+                this._Tag = tag;
+            }
+
+            public override string ToString() { return _Contents; }
+        }
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            {
+                XDocument xdoc = XDocument.Load("http://www.cbr.ru/scripts/XML_daily.asp?date_req=05/05/2020");
+                foreach (XElement phoneElement in xdoc.Element("ValCurs").Elements("Valute"))
+                {
+                    XAttribute id = phoneElement.Attribute("ID");
+                    XElement name = phoneElement.Element("Name");
+                    XElement priceElement = phoneElement.Element("Value");
+
+                    var ComboBoxItem = new ComboBoxItem(name.Value.ToString(), id.Value.ToString());
+
+                    comboBox1.Items.Add(ComboBoxItem);
+                }
+                comboBox1.SelectedIndex = 0;
+            }
+        }
+
+
+
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            DateTime? dtStart = dateTimePicker1.Value;
+            DateTime? dtEnd = dateTimePicker2.Value;
+
+            if (dtStart == null || dtEnd == null)
+            {
+                MessageBox.Show("Установите обе даты");
+                return;
+            }
+            if (dtStart >= dtEnd)
+            {
+                var a = dtStart;
+                dtStart = dtEnd;
+                dtEnd = a;
+            }
+
+            string stStart = dtStart.Value.ToString("dd/MM/yyyy");
+            string stEnd = dtEnd.Value.ToString("dd/MM/yyyy");
+            string valutaCode = ((ComboBoxItem)comboBox1.SelectedItem).Tag.ToString();
+            string valutaName = ((ComboBoxItem)comboBox1.SelectedItem).Contents.ToString();
+            string query = string.Concat("http://www.cbr.ru/scripts/XML_dynamic.asp?date_req1=",
+                stStart, "&date_req2=", stEnd, "&VAL_NM_RQ=", valutaCode);
+            XDocument xdoc = XDocument.Load(query);
+
+            chart1.Series.Clear();
+
+            chart1.Name = valutaName;
+            chart1.Series.Add("curr");
+            chart1.Series["curr"].ChartType = SeriesChartType.Line;
+            chart1.Series["curr"].XValueType = ChartValueType.DateTime;
+
+            foreach (XElement phoneElement in xdoc.Element("ValCurs").Elements("Record"))
+            {
+                XAttribute date = phoneElement.Attribute("Date");
+                XElement value = phoneElement.Element("Value");
+
+                double val = double.Parse(value.Value.ToString());
+                string strDate = date.Value.ToString();
+
+                DateTime dt = DateTime.ParseExact(strDate, "dd.MM.yyyy", CultureInfo.InvariantCulture);
+
+                chart1.Series["curr"].Points.AddXY(dt, val);
+
+            }
+            chart1.ChartAreas[0].AxisY.IsStartedFromZero = false;
+            chart1.ChartAreas[0].RecalculateAxesScale();
+        }
+
+            private void Form1_Resize(object sender, EventArgs e)
+        {
+            chart1.Size = new Size(this.Width - 300, this.Height - 100);
+        }
+    }
+}
