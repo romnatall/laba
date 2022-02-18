348919a256486c993390f5570c4d8c4641a5d297
429284-15 <429284-15>
2022-02-18 15:30:56 +0300

    создал

---
+++ Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿
+namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(44, 290);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "send";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(44, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(246, 36);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(481, 348);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(57, 62);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "start";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(57, 91);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "stop";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(17, 214);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(176, 70);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox3_KeyDown);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(82, 151);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 154);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "name";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(44, 10);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+
+        bool done = false;
+        UdpClient client;
+        IPAddress groupAddress;
+        int localPort;
+        int remotePort;
+        int ttl;
+
+        IPEndPoint remoteEP;
+        UnicodeEncoding encoding = new UnicodeEncoding();
+
+        string name;
+        string message;
+
+        SynchronizationContext _syncContext;
+
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Enabled = false;
+
+            groupAddress = IPAddress.Parse("234.5.6.11");
+
+            textBox1.Text = "8080";
+            textBox5.Text = "8080";
+            textBox4.Text = "Anon";
+
+            ttl = 32;
+            _syncContext = SynchronizationContext.Current;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            localPort = int.Parse(textBox1.Text);
+            remotePort = int.Parse(textBox5.Text);
+            try
+            {
+                name = textBox4.Text;
+                textBox4.ReadOnly = true;
+
+                client = new UdpClient(localPort);
+                client.JoinMulticastGroup(groupAddress, ttl);
+                remoteEP = new IPEndPoint(groupAddress, remotePort);
+
+                Thread receiver = new Thread(new ThreadStart(Listener));
+                receiver.IsBackground = true;
+                receiver.Start();
+                byte[] data = encoding.GetBytes(name + " has joined the chat");
+                client.Send(data, data.Length, remoteEP);
+                button2.Enabled = false;
+                button3.Enabled = true;
+                button1.Enabled = true;
+            }
+            catch(Exception er)
+            {
+                MessageBox.Show("error");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            StopListener();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        void Listener()
+        {
+            done = false;
+            try
+            {
+                while (!done)
+                {
+                    IPEndPoint ep = null;
+                    byte[] buffer = client.Receive(ref ep);
+                    message = encoding.GetString(buffer);
+                    _syncContext.Post(o => DisplayReceivedMessage(), null);
+                }
+            }
+            catch (Exception e)
+            {
+                if (done)
+                    return;
+            }
+
+        }
+
+        void DisplayReceivedMessage()
+        {
+            string time = DateTime.Now.ToString("t");
+            textBox2.Text += time + " " + message + "\r\n" + textBox3.Text+"\n";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string time = DateTime.Now.ToString("t");
+            if(textBox1.Text!=textBox5.Text)
+                textBox2.Text += time + " [" + name +"] "+ textBox3.Text + "\r\n";
+            try
+            {
+                byte[] data = encoding.GetBytes("["+name + "] " + textBox3.Text);
+                client.Send(data, data.Length, remoteEP);
+                textBox3.Clear();
+                textBox3.Focus();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, ex.Message, "Error MulticastChat");
+            }
+        }
+
+        void StopListener()
+        {
+            try
+            {
+                byte[] data = encoding.GetBytes(name + " has left the chart");
+                client.Send(data, data.Length, remoteEP);
+                client.DropMulticastGroup(groupAddress);
+                client.Close();
+                done = true;
+                button2.Enabled = true;
+                button3.Enabled = false;
+                button1.Enabled = false;
+                textBox4.ReadOnly = false;
+            }
+            catch(Exception e)
+            {
+                ;
+            }
+        }
+
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (!done)
+                StopListener();
+        }
+
+
+
+        private void textBox3_KeyDown(object sender, KeyEventArgs e)
+        {
+            e.Handled = false;
+            if (e.KeyCode == Keys.Enter)
+                button1_Click(null, null);
+            
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            textBox2.Size = new Size(this.Size.Width-300,this.Size.Height-100) ;
+        }
+    }
+}
